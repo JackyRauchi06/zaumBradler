0d20f797fe16e24d9c11fcee47517f5bcd1bbd83
JackyRauchi06 <jonas.rauchenberger@htlstp.at>
2023-06-13 15:19:59 +0200

    Projektbeginn

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,19 @@
 import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
 import {RouterModule, Routes} from "@angular/router";
 import {UserComponent} from "./user/user.component";
 import {AdminComponent} from "./admin/admin.component";
+import {ContactComponent} from "./user/contact/contact.component";
+import {HomeComponent} from "./user/home/home.component";
+import {LoginComponent} from "./user/login/login.component";
 
 const routes: Routes = [
   {path : 'user', component : UserComponent},
   {path : 'admin', component : AdminComponent},
+  {path : 'contact', component : ContactComponent},
+  {path : 'home', component : HomeComponent},
+  {path : 'login', component : LoginComponent},
   {path : '', pathMatch : 'full', redirectTo : 'user'}
+
 ]
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,24 @@ import { AppComponent } from './app.component';
 import { AdminComponent } from './admin/admin.component';
 import { AppRoutingModule } from './app-routing.module';
 import { UserComponent } from './user/user.component';
+import { HomeComponent } from './user/home/home.component';
+import { ContactComponent } from './user/contact/contact.component';
+import { LoginComponent } from './user/login/login.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent,
     AdminComponent,
-    UserComponent
+    UserComponent,
+    HomeComponent,
+    ContactComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
